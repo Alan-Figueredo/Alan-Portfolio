c1e2eb54c7353c751a98f7086fd0578c1eb6778e
Alan-Figueredo <afigueredo2000@gmail.com>
2022-04-07 16:30:11 -0300

    cambios

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ export const Footer = () =>{
     return(
         <>
             <Container id="footer" className="mb-4">
-                <Row>
+                <Row className="mb-3">
                     <div className="col-sm col-6 text-sm text-center">
                         <a href="https://github.com/Alan-Figueredo" className="mr-2" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} className="fab fa-github"/></a>
                     </div>
@@ -20,6 +20,9 @@ export const Footer = () =>{
                     <div className="col-sm text-center text-sm mt-sm mt-3">
                         <a href="mailto:afigueredo2000@gmail.com" id="mail">afigueredo2000@gmail.com</a>
                     </div>
+                </Row>
+                <Row>
+                    <a href="https://api.whatsapp.com/send?phone=+541151212855&text=%C2%A1Hola!%20Gracias%20por%20contactar%20conmigo.%20Deja%20tu%20mensaje,%20te%20respondere%20en%20breve." target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faWhatsapp} className="fa-whatsapp"/></a>
                 </Row>
             </Container>
         </>
