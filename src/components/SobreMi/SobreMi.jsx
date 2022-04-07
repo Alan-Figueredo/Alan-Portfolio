0155e2bc86476af7a59e67a137ffe5416ef247bd
Alan-Figueredo <afigueredo2000@gmail.com>
2022-04-07 16:33:34 -0300

    cambios

--- src/components/SobreMi/SobreMi.jsx
+++ src/components/SobreMi/SobreMi.jsx
@@ -2,7 +2,7 @@ import { Container,  Row } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./SobreMi.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faLinkedinIn, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 import { faCommentAlt, faStar } from "@fortawesome/free-solid-svg-icons";
 import Records from "../Records.json";
 
