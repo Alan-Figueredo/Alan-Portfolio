a46d054eb3c67ffc23d29b42a3bd654c4cd61e43
Alan-Figueredo <afigueredo2000@gmail.com>
2022-04-12 10:51:45 -0300

    cambios

--- src/components/SobreMi/SobreMi.jsx
+++ src/components/SobreMi/SobreMi.jsx
@@ -16,9 +16,9 @@ export const SobreMi = () =>{
                     <p>Mi nombre es Alan Figueredo y soy Desarrollador Web Front end. Soy un joven que busca su primera experiencia laboral en un equipo como desarrollador, para así seguir formandome, seguir aprendiendo y lograr mejorar mis habilidades tecnicas.</p>
                     <p>Abajo está mi stack tecnológico, como también mis estudios, idiomas que manejo y mis hobbies. </p>
                     <Row className="my-5">
-                        <a className="col-sm-1 col-2" href="https://github.com/Alan-Figueredo" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} className="fab fa-github"/></a>
-                        <a className="col-sm-4 col-3" href="https://www.linkedin.com/in/alan-figueredo/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedinIn} className="fab fa-linkedin-in"/></a>
-                        <a className="col-sm-4 col-6 nav-link cv2 text-center " href="https://drive.google.com/file/d/1xIfuSJzIMA7nmLj56PiUvRV77LbK0LjI/view?usp=sharing" target="_blank" rel="noopener noreferrer">Descargar CV</a>
+                        <a className="col-sm-1 col-6 text-sm text-center" href="https://github.com/Alan-Figueredo" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} className="fab fa-github"/></a>
+                        <a className="col-sm-4 col-6 text-sm text-center" href="https://www.linkedin.com/in/alan-figueredo/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedinIn} className="fab fa-linkedin-in"/></a>
+                        <a className="col-sm-4 col-10 mt-sm-0 mt-2 nav-link cv2 text-sm text-center " href="https://drive.google.com/file/d/1xIfuSJzIMA7nmLj56PiUvRV77LbK0LjI/view?usp=sharing" target="_blank" rel="noopener noreferrer">Descargar CV</a>
                     </Row>
                 </div>
                 <div className="col-12 mt-5 mb-4 mt-sm-0 col-sm-6 text-center">
@@ -53,12 +53,12 @@ export const SobreMi = () =>{
                 <div className="row my-5">
                     <div className="col-sm-6 col-12 mb-3 mt-3">
                         <div className="card shadow">
-                            <div className="row py-4 px-3"> 
+                            <div className="row pl-3"> 
                                 <div id="cardd" className="col-sm-3 col-5">
                                     <FontAwesomeIcon icon={faCommentAlt} className="fa-comment-alt text-center my-4"/> 
                                 </div>
                                 <div className="col">
-                                    <h3>IDIOMAS</h3>
+                                    <h3 className="mt-2">IDIOMAS</h3>
                                     <h4>INGLES- FCE</h4>
                                     <h4>ESPAÑOL-Nativo</h4>
                                 </div>
@@ -67,12 +67,12 @@ export const SobreMi = () =>{
                     </div>  
                     <div className="col-sm-6 col-12 mb-3 mt-3">
                         <div className="card shadow">
-                            <div className="row py-4 px-3"> 
+                            <div className="row pl-3"> 
                                 <div id="cardd" className="col-sm-3 col-5">
                                     <FontAwesomeIcon icon={faStar} className="fas fa-star text-center my-4"/>
                                 </div>
                                 <div className="col">
-                                    <h3>HOBBIES</h3>
+                                    <h3 className="mt-2">HOBBIES</h3>
                                     <h4>Bateria</h4>
                                     <h4>Videojuegos</h4>
                                     <h4>Programar</h4>
