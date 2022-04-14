8ac8b90b040fc68bcfde1d8f347ff31ca189e5e1
Alan-Figueredo <afigueredo2000@gmail.com>
2022-04-14 13:58:52 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -6,18 +6,11 @@ import { Proyectos } from "../src/components/ProyectosPage/Proyectos";
 import { NavBar } from "./components/NavBar/NavBar";
 import { Contacto } from "../src/components/Contacto/Contacto";
 import { Footer } from './components/Footer/Footer';
-import { Helmet } from "react-helmet";
 
 function App() {
   return (
   <>
     <Router>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Alan Figueredo Portfolio</title>
-        <link rel="canonical" href="http://mysite.com/example" />
-        <meta name="description" content="Alan Figueredo Portfolio" />
-      </Helmet>
       <NavBar/>
       <Routes>
           <Route index path="/" element={<Inicio/>}/>
