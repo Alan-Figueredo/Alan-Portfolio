234461e821013a4b8ff0f79d029a397641d3f490
Alan-Figueredo <afigueredo2000@gmail.com>
2022-04-14 17:09:22 -0300

    cambios

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,13 +8,7 @@ import { NavLink } from "react-router-dom";
 
 
 export const NavBar = () =>{
-    const estadoBarra = ()=>{
-        document.getElementById("nav-ul").removeClass("show")
-        document.getElementById("nav-ul").classList.toggle("hide");
-    }
-    const handleClick = ()=>{
-        estadoBarra();
-    }
+
     return(
         <div className="bg-black py-3">
             <Container>
@@ -28,16 +22,24 @@ export const NavBar = () =>{
                             </button>
                             <ul className="nav-bar nav nav-ul" id="nav-ul">
                                 <li className="nav-item">
-                                    <NavLink to="/" activeClassName="active" className=" nav-link  item"  onClick={()=>{handleClick()}}>Inicio</NavLink>
+                                    <NavLink to="/" activeClassName="active" className=" nav-link  item"  onClick={()=>{
+                                        document.getElementById("nav-ul").removeClass("show")
+                                        document.getElementById("nav-ul").classList.toggle("hide");}}>Inicio</NavLink>
                                 </li>
                                 <li className="nav-item ">
-                                    <NavLink to="sobre-mi" activeClassName="active" className=" nav-link item" onClick={()=>{handleClick()}}>Sobre mi</NavLink>
+                                    <NavLink to="sobre-mi" activeClassName="active" className=" nav-link item" onClick={()=>{
+                                        document.getElementById("nav-ul").removeClass("show")
+                                        document.getElementById("nav-ul").classList.toggle("hide");}}>Sobre mi</NavLink>
                                 </li>
                                 <li className="nav-item">
-                                    <NavLink to="proyectos" activeClassName="active" className=" nav-link  item" onClick={()=>{handleClick()}}>Proyectos</NavLink>
+                                    <NavLink to="proyectos" activeClassName="active" className=" nav-link  item" onClick={()=>{
+                                        document.getElementById("nav-ul").removeClass("show")
+                                        document.getElementById("nav-ul").classList.toggle("hide");}}>Proyectos</NavLink>
                                 </li> 
                                 <li className="nav-item">
-                                    <NavLink to="contacto" activeClassName="active" className=" nav-link  item" onClick={()=>{handleClick()}}>Contacto</NavLink>
+                                    <NavLink to="contacto" activeClassName="active" className=" nav-link  item" onClick={()=>{
+                                        document.getElementById("nav-ul").removeClass("show")
+                                        document.getElementById("nav-ul").classList.toggle("hide");}}>Contacto</NavLink>
                                 </li> 
                                 <li className="mt-1"><a href="https://drive.google.com/file/d/1xIfuSJzIMA7nmLj56PiUvRV77LbK0LjI/view?usp=sharing" target="_blank" rel="noopener noreferrer" id="cv">Descargá mi cv <FontAwesomeIcon icon={faDownload}/></a></li>
                             </ul>
