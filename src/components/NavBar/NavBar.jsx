21c989503c7b43d14f27e377e2e3e7376ec733aa
Figueredo <alan.figueredo@accenture.com>
2023-03-22 18:25:05 -0300

    s

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,7 +2,7 @@ import { Navbar } from "react-bootstrap"
 import { Container } from "react-bootstrap"
 import "./NavBar.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDownload, faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
